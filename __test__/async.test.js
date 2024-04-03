047dfaec4254f7f99da593f51ec405243e7b8651
DESKTOP-GMTST5N\Sunrise <pure90719@gmail.com>
2024-04-03 14:30:51 +0800

    first commit

---
+++ __test__/async.test.js
@@ -0,0 +1,77 @@
+import fetchData from '@/fetchData'
+
+/**
+ * Promises
+ */
+
+test('the data is peanut butter', () => {
+  const t = 'peanut butter'
+  return fetchData(t).then((data) => {
+    expect(data).toBe(t)
+  })
+})
+
+/**
+ * Async/Await
+ */
+
+test('the data is peanut butter', async () => {
+  const t = 'peanut butter'
+  const data = await fetchData(t)
+  expect(data).toBe(t)
+})
+
+test('the fetch fails with an error', async () => {
+  expect.assertions(1) // describing how many times of `expect()` will be executed
+  const e = new Error('error')
+  try {
+    await fetchData(e, true)
+  } catch (error) {
+    expect(error).toEqual(e)
+  }
+})
+
+// combining `async` and `await` with `.resolves` or `.rejects`...
+
+test('the data is peanut butter', async () => {
+  const t = 'peanut butter'
+  await expect(fetchData(t)).resolves.toBe(t)
+})
+
+test('the fetch fails with an error', async () => {
+  const e = new Error('error')
+  await expect(fetchData(e, true)).rejects.toEqual(e)
+})
+
+/**
+ * Callback
+ */
+
+test('the data is peanut butter', (done) => {
+  const t = 'peanut butter'
+  function callback(error, data) {
+    if (error) {
+      done(error)
+      return
+    }
+    try {
+      expect(data).toBe(t)
+      done()
+    } catch (error) {
+      done(error)
+    }
+  }
+  async function fetchData(callback) {
+    callback(null, t)
+  }
+
+  fetchData(callback)
+})
+
+/**
+ * .resolves / .rejects
+ */
+
+test('the data is peanut butter', () => {
+  return expect(fetchData)
+})
